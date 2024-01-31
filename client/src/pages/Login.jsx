aca35331b6fa99316a6a0484ae41fab2c5996d95
abbie930 <abbiecheng930@gmail.com>
2024-01-31 23:12:52 +0800

    feat: add register & login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,28 @@
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
+
 const Login = () => {
-  return ( <>Login</> );
+  return (
+    <>
+      <Form>
+        <Row style={{ height: '100vh', justifyContent: 'center', paddingTop: '10%' }}>
+          <Col xs={3}>
+            <Stack gap={3}>
+              <h3 style={{ textAlign: 'center' }}>Login</h3>
+              <Form.Control type="email" placeholder="email" />
+              <Form.Control type="password" placeholder="password" />
+              <Button type="submit" style={{ background: '#615d6e', borderStyle: 'none' }}>
+                Login
+              </Button>
+
+              <Alert variant="danger">
+                <p>Error</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
 }
- 
-export default Login;
+
+export default Login
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,29 @@
+import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
+
 const Register = () => {
-  return ( <>Register</> );
+  return (
+    <>
+      <Form>
+        <Row style={{ height: '100vh', justifyContent: 'center', paddingTop: '10%' }}>
+          <Col xs={3}>
+            <Stack gap={3}>
+              <h3 style={{ textAlign: "center" }}>Register</h3>
+              <Form.Control type="text" placeholder="Name" />
+              <Form.Control type="email" placeholder="email" />
+              <Form.Control type="password" placeholder="password" />
+              <Button type="submit" style={{ background: '#615d6e', borderStyle: 'none' }}>
+                Register
+              </Button>
+
+              <Alert variant="danger">
+                <p>Error</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
 }
- 
-export default Register;
+
+export default Register
