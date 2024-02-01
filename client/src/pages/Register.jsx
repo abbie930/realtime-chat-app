fef143120da63e8f9399f076474214553dc90225
abbie930 <abbiecheng930@gmail.com>
2024-02-01 20:03:41 +0800

    feat: add auth context

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,31 @@
+import { useContext } from 'react'
 import { Alert, Button, Form, Row, Col, Stack } from 'react-bootstrap'
+import { AuthContext } from '../context/AuthContext'
 
 const Register = () => {
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext)
   return (
     <>
       <Form>
         <Row style={{ height: '100vh', justifyContent: 'center', paddingTop: '10%' }}>
           <Col xs={3}>
             <Stack gap={3}>
-              <h3 style={{ textAlign: "center" }}>Register</h3>
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="email" />
-              <Form.Control type="password" placeholder="password" />
+              <h3 style={{ textAlign: 'center' }}>Register</h3>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) => updateRegisterInfo({ ...registerInfo, name: e.target.value })}
+              />
+              <Form.Control
+                type="email"
+                placeholder="email"
+                onChange={(e) => updateRegisterInfo({ ...registerInfo, email: e.target.value })}
+              />
+              <Form.Control
+                type="password"
+                placeholder="password"
+                onChange={(e) => updateRegisterInfo({ ...registerInfo, password: e.target.value })}
+              />
               <Button type="submit" style={{ background: '#615d6e', borderStyle: 'none' }}>
                 Register
               </Button>
