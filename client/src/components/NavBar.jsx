67fd520d7d58196ce280a512029df808b314bb2b
abbie930 <abbiecheng930@gmail.com>
2024-02-06 15:22:50 +0800

    feat: protecting routes and change the logging name in navbar page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react'
 import { Container, Nav, Navbar, Stack } from 'react-bootstrap'
 import { Link } from "react-router-dom"
+import { AuthContext } from '../context/AuthContext'
 
 const NavBar = () => {
+  
+  const { user } = useContext(AuthContext)
+
   return (
     <Navbar className="mb-4" style={{ background: "#4b4856" ,height: '3.75rem' }}>
       <Container>
@@ -12,7 +17,7 @@ const NavBar = () => {
         </h2>
         <Nav>
           <Stack direction="horizontal" gap={3}>
-            <span style={{ color: "#fac2a7"}}>Logged in as Abbie</span>
+            <span style={{ color: "#fac2a7"}}>Logged in as {user?.name}</span>
             <Link to="/login" className="link-light text-decoration-none">
               Login
             </Link>
