f94b97ba57c375e3fb1752d72725c2100bcbfd31
abbie930 <abbiecheng930@gmail.com>
2024-02-17 00:01:17 +0800

    feat: add fetch chats function

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,12 +1,12 @@
-export const baseUrl = "http://localhost:8000/api"
+export const baseUrl = 'http://localhost:8000/api'
 
 export const postRequest = async (url, body) => {
   const response = await fetch(url, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     },
-    body 
+    body
   })
 
   const data = await response.json()
@@ -14,10 +14,28 @@ export const postRequest = async (url, body) => {
   if (!response.ok) {
     let message
 
-    if(data?.message) {
+    if (data?.message) {
       message = data.message
     } else {
       message = data
+    }
+
+    return { error: true, message }
+  }
+
+  return data
+}
+
+export const getRequest = async (url) => {
+  const response = await fetch(url)
+
+  const data = await response.json()
+
+  if (!response.ok) {
+    let message = "An error occurred..."
+
+    if (data?.message) {
+      message = data.message
     }
 
     return { error: true, message }
